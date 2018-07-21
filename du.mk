f4bc458afecedb6fe1c836c96529f4102126b54f
Prashant Maheshwari <prashantqwerty4@gmail.com>
2018-07-21 11:16:06 +0530

    Galti Ho gai th

--- du.mk
+++ du.mk
@@ -15,12 +15,12 @@
 $(call inherit-product, device/yu/tomato/full_tomato.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/du/config/common.mk)
+$(call inherit-product, vendor/bootleggers/config/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
 
-PRODUCT_NAME := du_tomato
+PRODUCT_NAME := bootleg_tomato
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := tomato
 
