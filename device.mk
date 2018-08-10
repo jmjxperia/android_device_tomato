f9521f0b4eb3d7789677e44def25ddc441bb8eec
Prashant Maheshwari <prashantqwerty4@gmail.com>
2018-08-10 13:29:52 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -43,3 +43,7 @@ Jelly \
 # Charger
   PRODUCT_PACKAGES += \
     charger_res_images
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
